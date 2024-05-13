a21d378e9606e87726d5b75f7cc5bac7cb6f94aa
Anthony Serrano B <anthony.serrano.bianco@gmail.com>
2024-05-13 16:47:34 -0600

    Initial commit

---
+++ main.ino
@@ -0,0 +1,28 @@
+int relayPin = 4;
+int sensor_pin = A0;
+int output_value;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(relayPin, OUTPUT);
+  pinMode(sensor_pin, INPUT);
+  Serial.println("Reading from sensor...");
+  delay(2000);
+}
+
+void loop() {
+  output_value = analogRead(sensor_pin);
+  output_value = map(output_value, 550, 10, 0, 100);
+  Serial.print("Moisture: ");
+  Serial.print(output_value);
+  Serial.println("%");
+
+  if(output_value < 50) {
+    digitalWrite(relayPin, LOW);
+  }
+  else {
+    digitalWrite(relayPin, HIGH);
+  }
+
+  delay(1000);
+}
